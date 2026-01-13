d4204bb6a6f72d5b4ab8d3dd38e324a2f33b0ad1
Federico Emmanuel Garcia Gaitan <fegarcia@keldysh.physics.udel.edu>
2026-01-13 18:33:13 -0500

    Corrected errors

--- Kitaev_MC.jl
+++ Kitaev_MC.jl
@@ -36,8 +36,9 @@ function Build_H(N, hz, Jxy, Jz, sx_list, sy_list, sz_list)
     H = Single_term(H, Jxy, 7, 8, sy_list)
     H = Single_term(H, Jxy, 8, 9, sx_list)
     H = Single_term(H, Jxy, 9, 10, sy_list)
-    H = Single_term(H, Jz, 3, 8, sz_list)
     H = Single_term(H, Jz, 1, 10, sz_list)
+    H = Single_term(H, Jz, 3, 8, sz_list)
+ 
     return H
 end
 
@@ -95,7 +96,9 @@ function Construct_collapse(H, S, A)
     return C
 end
 
-function Integrate(N, hz, Jxy, Jz, θ, A, tlist, spin, NTRAJ)
+function Integrate(N, hz, Jxy, Jz, A, tlist, spin, NTRAJ)
+    SAVE_STRIDE = 20
+    saveat = tlist[1:SAVE_STRIDE:end]
     D_spin = Int(2 * spin + 1)
     si = to_sparse(qeye(D_spin) |> x -> Qobj(Matrix{ComplexF64}(x)))
     sx, sy, sz = Build_spinops(spin)
@@ -109,87 +112,104 @@ function Integrate(N, hz, Jxy, Jz, θ, A, tlist, spin, NTRAJ)
     P_plus1 = (I + Wp1) / 2
     P_plus2 = (I + Wp2) / 2
 
-    H = Build_H(N, hz, Jxy, Jz, θ, sx_list, sy_list, sz_list)
-    H_b = Build_H(N, 0, Jxy, Jz, θ, sx_list, sy_list, sz_list)
+    H = Build_H(N, hz, Jxy, Jz, sx_list, sy_list, sz_list)
 
     c_list = []
     for n in 1:N
-        push!(c_list, Construct_collapse(H_b, sx_list[n], A))
-        push!(c_list, Construct_collapse(H, sy_list[n], A))
-        push!(c_list, Construct_collapse(H, sz_list[n], A))
+        push!(c_list, Construct_collapse(H, sx_list[n], A))
     end
     evals, ekets = eigenstates(H, sparse = true)
 
     psi_init = ekets[1]
-    sz_list = vcat(sz_list, sy_list, sx_list)
 
-    # Include the correlation effects
-    for i in 1:N
-        for j in 1:i
-            push!(sz_list, sz_list[i]*sz_list[j])
-            push!(sz_list, sx_list[i]*sx_list[j])
-            push!(sz_list, sy_list[i]*sy_list[j])
-        end
-    end
-    push!(sz_list, Wp1)
-    push!(sz_list, Wp2)
-    push!(sz_list, H)
+    # Include the observables which will be computed a posteriori
+    e_ops = []
+    push!(e_ops, Wp1)
+    push!(e_ops, sz_list[1]*sz_list[10])
 
-    println(expect(Wp1, psi_init))
     println(expect(Wp2, psi_init))
 
-    println("Objects calculated, Initial wavefunction computed")
+    println("Objects calculated, Initial wavefunction computed. Starting MC unravelling")
 
-    println("Ready to solve lowrank approximation")
+    sol_lr = mcsolve(H, psi_init, tlist, c_list; ntraj=NTRAJ, saveat = saveat);
 
-    sol_lr = mcsolve(H, psi_init, tlist, c_list; e_ops = sz_list, ntraj=NTRAJ);
-
-    return sol_lr.expect, sol_lr.states
+    return e_ops, sol_lr.states
 end
 
-function Build_single(theta, phi, sx, sy, sz)
-    Sop = cos(theta) * sz + sin(theta) * cos(phi) * sx + sin(theta) * sin(phi) * sy
-    evals, ekets = eigenstates(Sop)
-    return ekets[1]
+function avg_reduced_dm(states, subsys, NTRAJ)
+    ρ_acc = nothing
+    for i in 1:NTRAJ
+        ρ_red = ptrace(states[i], subsys)
+
+        ρ_mat = Matrix{ComplexF64}(ρ_red.data)
+
+        if ρ_acc === nothing
+            ρ_acc = copy(ρ_mat)
+        else
+            ρ_acc .+= ρ_mat
+        end
+    end
+
+    ρ_acc ./= NTRAJ
+    return ρ_acc
 end
 
 function Run(N, hz, Jxy, Jz, A, spin, NTRAJ, filename)
-    Simulation_time = [5, 12, 50, 125, 500, 1250, 5000, 12500, 50000]
+    Simulation_time = 40000
     expval = 0
-    for i in 1:9
-        tlist = range(0, stop = 0.05*Simulation_time[i], length=Simulation_time[i])
-        expval, states= Integrate(N, hz, Jxy, Jz, A, tlist, spin, NTRAJ)
-        # The idea now is to check Takei's prediction on the localization of entanglement in loops
-        # according tothat idea C_half1 shouldn't present entanglement but C_half2 should
-        C_half1 = (1,2,8,9,10)
-        C_half2 = (1,2,3,8,9,10)
+    tlist = range(0, stop = 0.05*Simulation_time, length=Simulation_time)
+    SAVE_STRIDE = 20
+    saveat = tlist[1:SAVE_STRIDE:end]
+    expops, states= Integrate(N, hz, Jxy, Jz, A, tlist, spin, NTRAJ)
+    println(size(states[1][:]))
+    # States are being saved every few time steps
+    # Observables are computed as postprocessing operation
+    Nobs = 2000
+    W = zeros(Nobs)
+    SzSz = zeros(Nobs)
 
-        ρ = states[1][1]*states[1][1]'
-
-        for i in 1:(NTRAJ-1)
-            ρ += states[i+1][1]*states[i+1][1]'
+    for i in 1:Nobs
+        for j in 1:NTRAJ
+            W[i] += real(expect(expops[1], states[j][i]))
+            SzSz[i] += real(expect(expops[2], states[j][i]))
         end
-        ρ/=NTRAJ
-
-        # Here we compute the partial trace and then we compute the partial transpose to check for many body entanglement
-        ρ_p = ptrace(ρ, C_half1)
-        ρ_julia = Matrix{ComplexF64}(ρ_p)
-        aux_filename = "_"*string(i)*"_"*filename
-        outfileaux = "results/M1"*aux_filename*"_r.txt"
-        writedlm(outfileaux, real.(ρ_julia))
-        outfileaux = "results/M1"*aux_filename*"_i.txt"
-        writedlm(outfileaux, imag.(ρ_julia))
-
-
-        ρ_p = ptrace(ρ, C_half2)
-        ρ_julia = Matrix{ComplexF64}(ρ_p)
-        outfileaux = "results/M2"*aux_filename*"_r.txt"
-        writedlm(outfileaux, real.(ρ_julia))
-        outfileaux = "results/M2"*aux_filename*"_i.txt"
-        writedlm(outfileaux, imag.(ρ_julia))
+        W[i] /= NTRAJ
+        SzSz[i] /= NTRAJ
     end
 
-    return expval
+    println("Done computing observables")
+
+    #save data
+    outfilename = "results/W"*filename*".txt"
+    writedlm(outfilename, W)
+    outfilename = "results/SzSz"*filename*".txt"
+    writedlm(outfilename, SzSz)
+
+    # Now, lets compute the reduced density matrices in an optimal fashion
+    # Define subsystems
+    C_half1 = (1,2,8,9,10)
+    C_half2 = (1,2,3,8,9,10)
+
+    # Just compute reduced density matrices for a chosen set of points 
+    Rho_times = [1,2,5,10,20,50,100,200,500,1000,2000]
+    for (idx, k) in enumerate(Rho_times)
+        states_k = [states[j][k] for j in 1:NTRAJ]
+
+        rho1 = avg_reduced_dm(states_k, C_half1, NTRAJ)
+        aux_filename = "_"*string(idx)*"_"*filename
+        outfileaux = "results/M1"*aux_filename*"_r.txt"
+        writedlm(outfileaux, real.(rho1))
+        outfileaux = "results/M1"*aux_filename*"_i.txt"
+        writedlm(outfileaux, imag.(rho1))
+
+        rho2 = avg_reduced_dm(states_k, C_half2, NTRAJ) 
+        aux_filename = "_"*string(idx)*"_"*filename
+        outfileaux = "results/M2"*aux_filename*"_r.txt"
+        writedlm(outfileaux, real.(rho2))
+        outfileaux = "results/M2"*aux_filename*"_i.txt"
+        writedlm(outfileaux, imag.(rho2))
+    end
+    return
 end
 
 N = parse(Int64, ARGS[1])
@@ -201,11 +221,10 @@ NTRAJ = 1000
 
 A = readdlm("Coefs.txt")
 
-for i in 1:15
-    SZ = Run(N, hz, Jxy, Jz, A[i+2, :], Spin, NTRAJ, string(i)*"_theta"*string(θ))
+i = 1
+filename = "N_$(N)_T_$(i)_h_$(hz)_Jxy_$(Jxy)"
+Run(N, hz, Jxy, Jz, A[i+2, :], Spin, NTRAJ, filename)
 
-    outfile1 = "results/S_KSL_$(N)_N_$(i)_T_$(hz)_h_$(Jxy)_Jxy_$(Jz)_Jz.txt"
-    outfile2 = "results/NEG_KSL_$(N)_N_$(i)_T_$(hz)_h_$(Jxy)_Jxy_$(Jz)_Jz.txt"
-
-    writedlm(outfile1, real.(SZ))
-end
+i = 10 
+filename = "N_$(N)_T_$(i)_h_$(hz)_Jxy_$(Jxy)"
+Run(N, hz, Jxy, Jz, A[i+2, :], Spin, NTRAJ, filename)
